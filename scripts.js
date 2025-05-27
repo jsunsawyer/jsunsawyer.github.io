6fea752faa9223cf166584e5edeacb80a3f52d1d
Jason Sawyer <jsunsawyer@gmail.com>
2025-05-26 22:15:11 -0700

    Slow scrolling

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 let smoother = ScrollSmoother.create({
   wrapper: '#smooth-wrapper',
   content: '#smooth-content',
-  smooth: 4,
+  smooth: 7,
   effects: true,
 });
 
